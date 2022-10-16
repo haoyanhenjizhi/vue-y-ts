4efca5bc5157b2c81a2f361ac17c0d546ab6f241
yan12259 <haoyan@soarinfotech.com>
2022-10-17 03:17:10 +0800

    fix(press): 修改配置
    修改项目当中插件版本以及各个内容之间的兼容性修复

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import { createApp } from 'vue'
-import { globalRegister } from './global'
-
-import './service/axios_demo'
 
 import App from './App.vue'
 
@@ -11,12 +8,8 @@ import store from './store'
 const app = createApp(App)
 
 // 注册element-plus/其他
-app.use(globalRegister)
+
 app.use(router)
 app.use(store)
 
 app.mount('#app')
-
-// console.log(VUE_APP_BASE_URL)
-console.log(process.env.VUE_APP_BASE_URL)
-console.log(process.env.VUE_APP_BASE_NAME)
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Login</div>
+  <div class="login"></div>
 </template>
 
 <script lang="ts">
@@ -12,4 +12,4 @@ export default defineComponent({
 })
 </script>
 
-<style scoped></style>
+<style scoped lang="less"></style>
